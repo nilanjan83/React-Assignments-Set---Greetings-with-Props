b7d08fcedc8dba2270bf6582a1028db3956d067e
nilanjan83 <68443233+nilanjan83@users.noreply.github.com>
2020-12-12 00:37:42 +0530

    cghange

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,10 +4,10 @@ import "../styles/App.css";
 const welcome = (props) => {
   return (
     <>
-      <h1>"Hey !"{props.name}</h1>
+      <h1>Hey {props.name}!</h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
 };
 
-export default App;
+export default Welcome;
